29272086d992d9a49ace106812474e98f7cf6793
Aleksei Golubkov <nuriver@gmail.com>
2024-10-20 19:01:37 +0200

    chore: add env port variable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import http from 'http';
 import hasBaseUrl from './utils/hasBaseUrl';
 import requestHandlers from './controllers';
+import 'dotenv/config';
 
 // TODO: delete in config false for unused locals and args
 
 export const BASE_URL = '/api/users';
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const method = req.method as string;
@@ -25,5 +27,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000');
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
